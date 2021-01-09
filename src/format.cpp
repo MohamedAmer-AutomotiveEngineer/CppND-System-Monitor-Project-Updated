905de33fd48fda9ba73a327522b54707ce1e8004
MohamedAmer-AutomotiveEngineer <MohamedAmer-AutomotiveEngineer@users.noreply.github.com>
2021-01-09 20:02:12 +0200

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    unsigned char hour, minutes;
+    string hour_s, minutes_s, seconds_s;
+    seconds = seconds % (24 * 3600); 
+    hour = seconds; // 3600
+    seconds %= 3600;
+    minutes = seconds; // 60
+    seconds %= 60;
+    return (std::to_string(hour) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds));
+}
