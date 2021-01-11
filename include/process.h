202d1447579ebb10b640fe8f999a3f5978681648
MohamedAmer-AutomotiveEngineer <MohamedAmer-AutomotiveEngineer@users.noreply.github.com>
2021-01-11 14:41:34 +0200

    Update process.h

--- include/process.h
+++ include/process.h
@@ -15,10 +15,10 @@ class Process {
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  static int procIndex;
   Process(void);
   // TODO: Declare any necessary private members
  private:
- static int procIndex;
  int pid_;
 };
 
